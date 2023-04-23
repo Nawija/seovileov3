2b53cc6ae1d5619549e65a08aa8945361d48b6fa
Valkon <96625039+Nawija@users.noreply.github.com>
2023-04-23 08:29:25 +0200

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -143,7 +143,7 @@ const IndexPage = ({ data }) => {
                     Szybka <strong>Strona Internetowa</strong>
                 </h2>
                 <div className=" bg-gradient-to-r from-amber-400 w-[30%] md:w-[20%] h-1 rounded-3xl mb-12 mx-auto" />
-                <div className="flex flex-col bg-white lg:bg-transparent lg:mt-20 px-4 lg:flex-row py-6 md:pb-16">
+                <div className="flex flex-col bg-gray-800 lg:bg-transparent lg:mt-20 px-4 lg:flex-row py-6 md:pb-16">
                     <div className="sm:w-4/5 -mt-16 w-full lg:w-1/2 mx-auto lg:my-auto flex mb-8 items-center justify-center lg:order-1">
                         <StaticImage
                             className="lg:-mt-20"
@@ -157,10 +157,10 @@ const IndexPage = ({ data }) => {
                         <p class=" px-2 md:px-12 font-bold text-emerald-700 text-sm">
                             TECHNOLOGIA GATSBY
                         </p>
-                        <p class=" px-2 md:px-12 text-gray-700 font-bold text-xl mb-4 md:mb-0">
+                        <p class=" px-2 md:px-12 text-gray-100 lg:text-gray-700 font-bold text-xl mb-4 md:mb-0">
                             UZYSKAJ NAJSZYBSZĄ STRONĘ INTERNETOWĄ!
                         </p>
-                        <p class=" px-2 md:px-12 md:mt-4 text-gray-700">
+                        <p class=" px-2 md:px-12 md:mt-4 text-gray-300 lg:text-gray-700">
                             Nasze strony internetowe tworzymy w technologii
                             Gatsby, aby zapewnić naszym klientom najlepsze
                             doświadczenie użytkownika. Dzięki szybkości i
@@ -170,7 +170,7 @@ const IndexPage = ({ data }) => {
                             pozwala nam łączyć dane z wielu źródeł i tworzyć
                             dynamiczne strony internetowe.
                         </p>
-                        <p class=" px-2 md:px-12 pb-6 mt-3 md:mt-4 text-gray-700">
+                        <p class=" px-2 md:px-12 pb-6 mt-3 md:mt-4 text-gray-300 lg:text-gray-700">
                             Optymalizacja SEO sprawia, że nasze strony są łatwo
                             widoczne dla wyszukiwarek i znajdują się na szczycie
                             wyników wyszukiwania. Wsparcie i dobre praktyki, na
@@ -190,7 +190,7 @@ const IndexPage = ({ data }) => {
                     xmlns="http://www.w3.org/2000/svg"
                 >
                     <path
-                        className=" fill-white lg:fill-[#f1f1f1]"
+                        className=" fill-[#1f2937] lg:fill-[#f1f1f1]"
                         d="M0 116.001C0 116.001 458.5 31.5011 1002 116.001C1545.5 200.501 2004 116.001 2004 116.001V9.15527e-05H0V116.001Z"
                     />
                 </svg>
@@ -380,178 +380,6 @@ const IndexPage = ({ data }) => {
                                     out four loko. 3 wolf moon brooklyn.
                                 </p>
                             </div>
-                        </div>
-                    </div>
-                </div>
-            </section>
-
-            <section class="pb-40 overflow-hidden relative">
-                <div className="absolute inset-0 -z-20 w-full h-full bg-gradient-to-t from-[#1f2d57] to-[#0C1530]" />
-                <svg
-                    className="absolute -top-2 w-full left-0 z-10"
-                    viewBox="0 0 2004 75"
-                    fill="none"
-                    xmlns="http://www.w3.org/2000/svg"
-                >
-                    <path
-                        d="M0 50.8259C0 50.8259 734 103 1002 50.8259C1270 -1.34822 2004 50.8259 2004 50.8259V0.967644H0V50.8259Z"
-                        fill="white"
-                    />
-                </svg>
-                <svg
-                    className="absolute -bottom-2 w-full left-0 z-10"
-                    viewBox="0 0 2004 75"
-                    fill="none"
-                    xmlns="http://www.w3.org/2000/svg"
-                >
-                    <path
-                        d="M2004 24.1561C2004 24.1561 1270 -28.018 1002 24.1561C734 76.3303 0 24.1561 0 24.1561V74.0144H2004V24.1561Z"
-                        fill="white"
-                    />
-                </svg>
-
-                <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    class="absolute block w-full"
-                    style={{ margin: "auto", "z-index": "-10;" }}
-                    height="680"
-                    preserveAspectRatio="none"
-                    viewBox="0 0 1920 880"
-                >
-                    <g transform="translate(960,440) scale(1,1) translate(-960,-440)">
-                        <linearGradient
-                            id="lg-0.047955344060927496"
-                            x1="0"
-                            x2="1"
-                            y1="0"
-                            y2="0"
-                        >
-                            <stop
-                                stop-color="hsl(221, 61%, 13%)"
-                                offset="0"
-                            ></stop>
-                            <stop
-                                stop-color="hsl(221, 46%, 36%)"
-                                offset="1"
-                            ></stop>
-                        </linearGradient>
-                        <path
-                            d=""
-                            fill="url(#lg-0.047955344060927496)"
-                            opacity="0.4"
-                        >
-                            <animate
-                                attributeName="d"
-                                dur="33.333333333333336s"
-                                repeatCount="indefinite"
-                                keyTimes="0;0.333;0.667;1"
-                                calcmod="spline"
-                                keySplines="0.2 0 0.2 1;0.2 0 0.2 1;0.2 0 0.2 1"
-                                begin="0s"
-                                values="M0 0L 0 804.2328934685746Q 320 597.3613372284876  640 571.0708916590191T 1280 512.0661063245175T 1920 301.8788007488083L 1920 0 Z;M0 0L 0 877.6839081951588Q 320 668.0720922803877  640 649.0018928349388T 1280 328.7087077664202T 1920 162.95038242563396L 1920 0 Z;M0 0L 0 724.9886210051687Q 320 661.4364572061575  640 623.2173947479624T 1280 359.20353038907734T 1920 135.51673041732283L 1920 0 Z;M0 0L 0 804.2328934685746Q 320 597.3613372284876  640 571.0708916590191T 1280 512.0661063245175T 1920 301.8788007488083L 1920 0 Z"
-                            ></animate>
-                        </path>
-                        <path
-                            d=""
-                            fill="url(#lg-0.047955344060927496)"
-                            opacity="0.4"
-                        >
-                            <animate
-                                attributeName="d"
-                                dur="33.333333333333336s"
-                                repeatCount="indefinite"
-                                keyTimes="0;0.333;0.667;1"
-                                calcmod="spline"
-                                keySplines="0.2 0 0.2 1;0.2 0 0.2 1;0.2 0 0.2 1"
-                                begin="-6.666666666666667s"
-                                values="M0 0L 0 765.7607191473613Q 320 641.7853945676919  640 624.2534689988059T 1280 365.27264408032966T 1920 190.38947978522663L 1920 0 Z;M0 0L 0 842.1984196370487Q 320 570.6690721707517  640 540.6844954979398T 1280 439.92879442880593T 1920 200.29713960445451L 1920 0 Z;M0 0L 0 796.6802345094818Q 320 721.9216894353016  640 696.8815669355181T 1280 373.6367381440213T 1920 196.63169821789495L 1920 0 Z;M0 0L 0 765.7607191473613Q 320 641.7853945676919  640 624.2534689988059T 1280 365.27264408032966T 1920 190.38947978522663L 1920 0 Z"
-                            ></animate>
-                        </path>
-                        <path
-                            d=""
-                            fill="url(#lg-0.047955344060927496)"
-                            opacity="0.4"
-                        >
-                            <animate
-                                attributeName="d"
-                                dur="33.333333333333336s"
-                                repeatCount="indefinite"
-                                keyTimes="0;0.333;0.667;1"
-                                calcmod="spline"
-                                keySplines="0.2 0 0.2 1;0.2 0 0.2 1;0.2 0 0.2 1"
-                                begin="-13.333333333333334s"
-                                values="M0 0L 0 801.7562714943509Q 320 634.0247183381232  640 605.7090791951217T 1280 503.9393370140325T 1920 224.7551247480177L 1920 0 Z;M0 0L 0 821.0401780336218Q 320 670.8690783540507  640 637.0744123031742T 1280 456.40745286432224T 1920 278.1294357804296L 1920 0 Z;M0 0L 0 744.0534225112256Q 320 637.6425395409125  640 593.2079605185819T 1280 457.03995196824286T 1920 254.87693899994804L 1920 0 Z;M0 0L 0 801.7562714943509Q 320 634.0247183381232  640 605.7090791951217T 1280 503.9393370140325T 1920 224.7551247480177L 1920 0 Z"
-                            ></animate>
-                        </path>
-                        <path
-                            d=""
-                            fill="url(#lg-0.047955344060927496)"
-                            opacity="0.4"
-                        >
-                            <animate
-                                attributeName="d"
-                                dur="33.333333333333336s"
-                                repeatCount="indefinite"
-                                keyTimes="0;0.333;0.667;1"
-                                calcmod="spline"
-                                keySplines="0.2 0 0.2 1;0.2 0 0.2 1;0.2 0 0.2 1"
-                                begin="-20s"
-                                values="M0 0L 0 817.8603658675457Q 320 592.9404308081629  640 559.1126621853513T 1280 428.9912604821798T 1920 209.017381620229L 1920 0 Z;M0 0L 0 802.0504889976935Q 320 561.3963273210122  640 537.6024084387631T 1280 430.41283267566695T 1920 256.1972069733954L 1920 0 Z;M0 0L 0 789.4448177495887Q 320 561.9675446430498  640 531.6192318019404T 1280 414.76018143244175T 1920 265.9163329632971L 1920 0 Z;M0 0L 0 817.8603658675457Q 320 592.9404308081629  640 559.1126621853513T 1280 428.9912604821798T 1920 209.017381620229L 1920 0 Z"
-                            ></animate>
-                        </path>
-                        <path
-                            d=""
-                            fill="url(#lg-0.047955344060927496)"
-                            opacity="0.4"
-                        >
-                            <animate
-                                attributeName="d"
-                                dur="33.333333333333336s"
-                                repeatCount="indefinite"
-                                keyTimes="0;0.333;0.667;1"
-                                calcmod="spline"
-                                keySplines="0.2 0 0.2 1;0.2 0 0.2 1;0.2 0 0.2 1"
-                                begin="-26.666666666666668s"
-                                values="M0 0L 0 844.0541574423102Q 320 623.0697081316591  640 592.8483890737847T 1280 469.85448734523794T 1920 190.81850676853674L 1920 0 Z;M0 0L 0 871.4928294956283Q 320 618.9784567388518  640 593.1183717103518T 1280 376.5051942642811T 1920 141.32293927545027L 1920 0 Z;M0 0L 0 782.0118384610068Q 320 727.3267836497654  640 694.0476176759635T 1280 518.1545471640493T 1920 276.0053882957168L 1920 0 Z;M0 0L 0 844.0541574423102Q 320 623.0697081316591  640 592.8483890737847T 1280 469.85448734523794T 1920 190.81850676853674L 1920 0 Z"
-                            ></animate>
-                        </path>
-                    </g>
-                </svg>
-
-                <div class="flex items-center justify-center mx-auto px-6 md:px-12 xl:px-32">
-                    <div class="text-center text-gray-700">
-                        <div
-                            class="block rounded-lg shadow-xl px-6 py-10 md:py-12 md:px-10"
-                            style={{
-                                "margin-top": "180px",
-                                "background-color": "hsla(0, 0%, 100%, 0.8)",
-                                "backdrop-filter": "saturate(200%) blur(8px)",
-                            }}
-                        >
-                            <h3 class="text-xl md:text-2xl xl:text-3xl font-semibold tracking-tight mb-6">
-                                Zdobądz Przewage dzięki <br />
-                                <strong class="text-amber-500 mt-1 lg:drop-shadow-md">
-                                    Stronie Internetowej
-                                </strong>
-                            </h3>
-                            <Link
-                                to="/projekty"
-                                class="btnOrange mr-4"
-                                data-mdb-ripple="true"
-                                data-mdb-ripple-color="light"
-                                role="button"
-                            >
-                                Projekty
-                            </Link>
-                            <Link
-                                to="/kontakt"
-                                class="inline-block px-7 py-3 font-semibold text-sm hover:shadow-lg text-gray-900 leading-tight uppercase rounded hover:text-gray-700 bg-gray-100 focus:bg-gray-100 shadow-md focus:outline-none focus:ring-0 active:bg-gray-200 transition duration-200"
-                                data-mdb-ripple="true"
-                                data-mdb-ripple-color="light"
-                                role="button"
-                            >
-                                Kontakt &#8594;
-                            </Link>
                         </div>
                     </div>
                 </div>
