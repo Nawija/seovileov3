80083fe44b840a856a8a91a80dfa0139c22b8452
Valkon <96625039+Nawija@users.noreply.github.com>
2023-05-04 18:16:47 +0200

    Update budowa.js

--- src/pages/budowa.js
+++ src/pages/budowa.js
@@ -36,7 +36,7 @@ const Budowa = () => (
         <section className="mb-52 mt-32 w-full flex flex-col items-center justify-center">
             <Link
                 to="/"
-                className="flex items-center mb-12 justify-center"
+                className="flex items-center -ml-20 mb-12 justify-center"
                 aria-label="logo"
             >
                 <div className="h-auto w-16 ">
@@ -48,17 +48,19 @@ const Budowa = () => (
                         src="../assets/logoBrowser.png"
                     />
                 </div>
-                <p className="text-gray-700 text-2xl ml-2 tracking-wide font-semibold">
+                <p className="text-gray-700 text-2xl ml-2 -tracking-wide font-semibold">
                     Seovileo
                 </p>
             </Link>
 
-            <p className="text-gray-700 text-xl tracking-wide font-semibold">
-                Strona w budownie
-            </p>
-            <p className="text-gray-700 text-xl tracking-wide font-semibold">
-                Zapraszam za jakis czas
-            </p>
+            <div className="mb-12">
+                <p className="text-gray-700 text-xl tracking-wide font-semibold">
+                    Strona w budownie
+                </p>
+                <p className="text-gray-700 text-xl tracking-wide font-semibold">
+                    Zapraszam za jakis czas
+                </p>
+            </div>
 
             <div className="flex items-center justify-center">
                 <Link to="/" className="btn mr-6">
