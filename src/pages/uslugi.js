ec6cce8a7299ead859f123ab252660060d0d3ab5
Valkon <96625039+Nawija@users.noreply.github.com>
2023-06-22 19:40:28 +0200

    Update uslugi.js

--- src/pages/uslugi.js
+++ src/pages/uslugi.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { useStaticQuery, graphql, Link } from "gatsby";
 import { GatsbyImage, getImage } from "gatsby-plugin-image";
 
@@ -24,17 +24,18 @@ const Usługi = () => {
             }
         }
     `);
-
+    const [showFaq, setFaq] = useState(false);
+    const handeFaq = () => setFaq(!showFaq);
     return (
         <Layout>
             <HeroLinks />
             <section className="relative">
-                <div className="mx-auto max-w-screen-xl px-4 md:px-8">
-                    <h3 className="text-center sm:text-start text-xl md:text-2xl mb-4 text-amber-600 font-bold tracking-wide ">
+                <div className="mx-auto max-w-screen-xl px-4 md:px-8 -mt-10">
+                    <h3 className="text-center text-xl md:text-2xl mb-4 text-[#006647] font-bold tracking-wide ">
                         Usługi
                     </h3>
-                    <div className="flex flex-col sm:flex-row mb-12 justify-between gap-8 items-center">
-                        <p className="max-w-screen-sm text-center sm:text-start text-gray-900 lg:text-base">
+                    <div className="mb-12 max-w-screen-sm mx-auto text-center">
+                        <p className=" text-gray-900 lg:text-base">
                             Oferuję szeroki zakres usług związanych z
                             projektowaniem i tworzeniem szybkich stron
                             internetowych
@@ -71,6 +72,203 @@ const Usługi = () => {
                     </div>
                 </div>
             </section>
+            <div class="bg-white py-6 sm:py-8 lg:py-12">
+                <div class="mx-auto max-w-screen-2xl px-4 md:px-8">
+                    <div class="mb-10 md:mb-16">
+                        <h2 class="mb-4 text-center text-2xl font-bold text-gray-800 md:mb-6 lg:text-3xl">
+                            Frequently asked questions
+                        </h2>
+
+                        <p class="mx-auto max-w-screen-md text-center text-gray-700 md:text-lg">
+                            This is a section of some simple filler text, also
+                            known as placeholder text. It shares some
+                            characteristics of a real written text but is random
+                            or otherwise generated.
+                        </p>
+                    </div>
+
+                    <div class="mx-auto flex max-w-screen-sm flex-col border-t">
+                        <div class="border-b">
+                            <button
+                                onClick={handeFaq}
+                                class="flex cursor-pointer w-full justify-between gap-2 py-4 text-black hover:text-indigo-700 active:text-indigo-600"
+                            >
+                                <span class="font-semibold transition duration-100 md:text-lg">
+                                    How does the product work?
+                                </span>
+
+                                <span
+                                    class={`text-indigo-700 transition-transform ${
+                                        !showFaq ? "rotate-0" : "rotate-180"
+                                    }`}
+                                >
+                                    <svg
+                                        xmlns="http://www.w3.org/2000/svg"
+                                        class="h-6 w-6"
+                                        fill="none"
+                                        viewBox="0 0 24 24"
+                                        stroke="currentColor"
+                                    >
+                                        <path
+                                            stroke-linecap="round"
+                                            stroke-linejoin="round"
+                                            stroke-width="2"
+                                            d="M19 9l-7 7-7-7"
+                                        />
+                                    </svg>
+                                </span>
+                            </button>
+
+                            <p
+                                class={`text-gray-700 overflow-hidden transition-all ${
+                                    !showFaq ? "h-0" : "h-max mb-4"
+                                }`}
+                            >
+                                This is a section of some simple filler text,
+                                also known as placeholder text. It shares some
+                                characteristics of a real written text but is
+                                random or otherwise generated. It may be used to
+                                display a sample of fonts or generate text for
+                                testing.
+                            </p>
+                        </div>
+
+                        <div class="border-b">
+                            <button
+                                onClick={handeFaq}
+                                class="flex w-full cursor-pointer justify-between gap-2 py-4 text-black hover:text-indigo-700 active:text-indigo-600"
+                            >
+                                <span class="font-semibold transition duration-100 md:text-lg">
+                                    What are the features?
+                                </span>
+
+                                <span
+                                    class={`text-indigo-700 transition-transform ${
+                                        !showFaq ? "rotate-0" : "rotate-180"
+                                    }`}
+                                >
+                                    <svg
+                                        xmlns="http://www.w3.org/2000/svg"
+                                        class="h-6 w-6"
+                                        fill="none"
+                                        viewBox="0 0 24 24"
+                                        stroke="currentColor"
+                                    >
+                                        <path
+                                            stroke-linecap="round"
+                                            stroke-linejoin="round"
+                                            stroke-width="2"
+                                            d="M19 9l-7 7-7-7"
+                                        />
+                                    </svg>
+                                </span>
+                            </button>
+
+                            <p
+                                class={`text-gray-700 overflow-hidden transition-all ${
+                                    showFaq ? "h-0" : "h-max mb-4"
+                                }`}
+                            >
+                                This is a section of some simple filler text,
+                                also known as placeholder text. It shares some
+                                characteristics of a real written text but is
+                                random or otherwise generated. It may be used to
+                                display a sample of fonts or generate text for
+                                testing.
+                            </p>
+                        </div>
+
+                        <div class="border-b">
+                            <button
+                                onClick={handeFaq}
+                                class="flex cursor-pointer w-full justify-between gap-2 py-4 text-black hover:text-indigo-700 active:text-indigo-600"
+                            >
+                                <span class="font-semibold transition duration-100 md:text-lg">
+                                    What about integrations?
+                                </span>
+
+                                <span
+                                    class={`text-indigo-700 transition-transform ${
+                                        !showFaq ? "rotate-0" : "rotate-180"
+                                    }`}
+                                >
+                                    <svg
+                                        xmlns="http://www.w3.org/2000/svg"
+                                        class="h-6 w-6"
+                                        fill="none"
+                                        viewBox="0 0 24 24"
+                                        stroke="currentColor"
+                                    >
+                                        <path
+                                            stroke-linecap="round"
+                                            stroke-linejoin="round"
+                                            stroke-width="2"
+                                            d="M19 9l-7 7-7-7"
+                                        />
+                                    </svg>
+                                </span>
+                            </button>
+
+                            <p
+                                class={`text-gray-700 overflow-hidden transition-all ${
+                                    showFaq ? "h-0" : "h-max mb-4"
+                                }`}
+                            >
+                                This is a section of some simple filler text,
+                                also known as placeholder text. It shares some
+                                characteristics of a real written text but is
+                                random or otherwise generated. It may be used to
+                                display a sample of fonts or generate text for
+                                testing.
+                            </p>
+                        </div>
+                        <div class="border-b">
+                            <button
+                                onClick={handeFaq}
+                                class="flex cursor-pointer w-full justify-between gap-2 py-4 text-black hover:text-indigo-700 active:text-indigo-600"
+                            >
+                                <span class="font-semibold transition duration-100 md:text-lg">
+                                    Is support available?
+                                </span>
+
+                                <span
+                                    class={`text-indigo-700 transition-transform ${
+                                        !showFaq ? "rotate-0" : "rotate-180"
+                                    }`}
+                                >
+                                    <svg
+                                        xmlns="http://www.w3.org/2000/svg"
+                                        class="h-6 w-6"
+                                        fill="none"
+                                        viewBox="0 0 24 24"
+                                        stroke="currentColor"
+                                    >
+                                        <path
+                                            stroke-linecap="round"
+                                            stroke-linejoin="round"
+                                            stroke-width="2"
+                                            d="M19 9l-7 7-7-7"
+                                        />
+                                    </svg>
+                                </span>
+                            </button>
+
+                            <p
+                                class={`text-gray-700 overflow-hidden transition-all ${
+                                    showFaq ? "h-0" : "h-max mb-4"
+                                }`}
+                            >
+                                This is a section of some simple filler text,
+                                also known as placeholder text. It shares some
+                                characteristics of a real written text but is
+                                random or otherwise generated. It may be used to
+                                display a sample of fonts or generate text for
+                                testing.
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </Layout>
     );
 };
